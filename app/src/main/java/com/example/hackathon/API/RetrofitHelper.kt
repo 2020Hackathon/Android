db3d8d8929a40905a808ceb2d7dac87023420082
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-25 06:37:04 +0900

    <Android> BaseUrl

--- app/src/main/java/com/example/hackathon/API/RetrofitHelper.kt
+++ app/src/main/java/com/example/hackathon/API/RetrofitHelper.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitHelper{
     private var retrofit = Retrofit.Builder()
-        .baseUrl("http://54.180.119.69")
+        .baseUrl(BaseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     fun getUserAPI() : UserAPI{
