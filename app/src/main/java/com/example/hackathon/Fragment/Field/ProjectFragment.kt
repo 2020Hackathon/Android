72521ff0a9320d8d499ffe1bb28f1a6d9f258a3d
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-24 17:40:50 +0900

    <Create> Fragment

---
+++ app/src/main/java/com/example/hackathon/Fragment/Field/ProjectFragment.kt
@@ -0,0 +1,26 @@
+package com.example.hackathon.Fragment.Field
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hackathon.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class ProjectFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_project, container, false)
+
+
+
+
+        return  view
+    }
+}
---
+++ app/src/main/java/com/example/hackathon/Fragment/Field/Self_introductionFragment.kt
@@ -0,0 +1,19 @@
+package com.example.hackathon.Fragment.Field
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hackathon.R
+
+class Self_introductionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_self_introduction, container, false)
+    }
+
+}
